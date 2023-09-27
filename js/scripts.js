6131dd6b72ad75373ddcae97378eb556da1f191f
DWilken02 <Dylan.Wilken06@gmail.com>
2023-09-27 15:51:52 -0700

    WIP add more to html and JS

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+  let form = document.querySelector("form");
+  form.onsubmit = function(event) {
+    event.preventDefault();
+    const triangle1Input = document.getElementById("triangle1Input").value;
+    const triangle2Input = document.getElementById("triangle2Input").value;
+    const triangle3Input = document.getElementById("triangle3Input").value;
+    
+    document.querySelector("")
+  }
+}
