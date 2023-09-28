6a65ec3fc501b187babf4a918ed1fe211d68ee54
DWilken02 <Dylan.Wilken06@gmail.com>
2023-09-28 15:55:32 -0700

    simplify JavaScript and update HTML

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,27 @@
+//Business Logic
+function checkType(side1, side2, side3) {
+  if (side1 <= 0 || side2 <= 0 || side3 <= 0) {
+    return "This is not a triangle";
+  } else if (side1 === side2 && side3 === side1) {
+    return "This triangle is Equilateral";
+  } else if (side2 === side1 || side3 === side2 || side1 === side3) {
+    return "This triangle is Isosceles";
+  } else if (side3 !== side1 && side3 !== side2 && side1 !== side2) {
+    return "This triangle is Scalene";
+  } else {
+    return "This is not a triangle";
+  }
+}
+
+//UI Logic
 window.onload = function() {
   let form = document.querySelector("form");
   form.onsubmit = function(event) {
     event.preventDefault();
-    const triangle1Input = document.getElementById("triangle1Input").value;
-    const triangle2Input = document.getElementById("triangle2Input").value;
-    const triangle3Input = document.getElementById("triangle3Input").value;
-    
-    document.querySelector("")
+    const side1Length = parseInt(document.getElementById("triangle1Input").value);
+    const side2Length = parseInt(document.getElementById("triangle2Input").value);
+    const side3Length = parseInt(document.getElementById("triangle3Input").value);
+    const triangleType = checkType(side1Length, side2Length, side3Length);
+    document.getElementById("output").innerText = triangleType;
   }
 }
